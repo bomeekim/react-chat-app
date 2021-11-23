ec0035e379406fcf26bae15e903e3cfd3d069aa2
bomeekim <thespringg@naver.com>
2021-11-23 14:55:43 +0900

    채팅 목록 상단 헤더 구현 및 제플린 가이드 적용

--- src/components/ChatList.js
+++ src/components/ChatList.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import ChatListItem from './ChatListItem'
+import style from '../css/ChatList.module.css';
+import menuIcon from '../assets/icons/rectangle_2.png';
+import accountIcon from '../assets/icons/rectangle.png';
+
+function Header() {
+  return (
+    <header className={style.header}>
+      <div className={style.header__content}>
+        <button type="button">
+          <img src={menuIcon} />
+        </button>
+        <div className={style.header__title}>채팅</div>
+        <button type="button">
+          <img src={accountIcon} />
+        </button>
+      </div> 
+    </header>
+  )
+}
 
 function ChatList() {
   return (
     <div>
-      <h1>ChatList</h1>
+      {/* <header className={styles.header} /> */}
+      <Header />
       <ChatListItem />
     </div>
   )
