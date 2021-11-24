8870e92106e5dec344b62e0e99c217bb9a423329
bomeekim <thespringg@naver.com>
2021-11-24 10:44:46 +0900

    채팅 목록 메시지 보낸 시간을 UTC -> locale 로 변환

--- src/components/ChatListItem.js
+++ src/components/ChatListItem.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import style from '../css/ChatListItem.module.css';
 
 function ChatListItem({ item }) {
-  console.log(item);
   const { 
     imageUrl,
     lastSentDateTime, 
@@ -10,6 +9,35 @@ function ChatListItem({ item }) {
     senderName, 
     unreadMessageCount 
   } = item;
+
+  /** 
+   * UTC 를 locale 에 맞는 시간으로 변경해주는 함수
+   * @param dateTime 메시지를 보낸 시간
+   */
+  const convertedDateTime = (dateTime) => {
+    const date = new Date(dateTime);
+    const now = new Date();
+
+    // dateTime이 없는 경우 현재 시간으로 대체한다.
+    // hh:mm 형태를 나타내기 위해 roomId 1,2 에 해당하는 데이터의 lastSentDateTime 에 "" 을 넣어둠
+    if (!dateTime) {
+      let hour = now.getHours();
+      let minute = now.getMinutes();
+
+      hour = hour >= 10 ? hour : `0${hour}`;
+      minute = minute >= 10 ? minute : `0${minute}`;
+
+      return `${hour}:${minute}`;
+    } else {
+      const weekday = [ '일', '월', '화', '수', '목', '금', '토' ];
+      const isOverWeek = now.getDate() - date.getDate() >= weekday.length;
+
+      // 현재 날짜기준 일주일이 지난 경우 mm월 dd일로 보여준다.
+      let month = date.getMonth();
+      month = month >= 10 ? month : `0${month}`;
+      return isOverWeek ? `${month + 1}월 ${date.getDate()}일` : `${weekday[date.getDay()]}요일`;
+    }
+  }
 
   return (
     <div className={style['list-item']}>
@@ -29,8 +57,7 @@ function ChatListItem({ item }) {
       </div>
       <div className={style['list-item__action']}>
         <div className={style['lite-item__action-text']}>
-          {/* TODO UTC -> Local Time */}
-          {lastSentDateTime}
+          {convertedDateTime(lastSentDateTime)}
         </div>
 
         {/* 안읽은 메시지 수가 있는 경우에만 보여준다. */}
